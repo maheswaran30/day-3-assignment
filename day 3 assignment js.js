6264f82196b2293820b352db5e01c11c92412d6c
maheswaran30 <45141153+maheswaran30@users.noreply.github.com>
2021-09-10 21:19:20 +0530

    Add files via upload

---
+++ day 3 assignment js.js
@@ -0,0 +1,23 @@
+
+alert("day 3 assignment");
+
+
+function addTask(){
+    let data=document.getElementById('inp').value;
+    document.getElementById('two').innerHTML+=`<h1 class='todo'>${data}</h1>`;
+    document.getElementById('inp').value="";
+}
+
+function changeColor(){
+
+    document.getElementById('two').style.backgroundColor="darkgreen";
+
+}
+
+
+
+
+
+
+
+
